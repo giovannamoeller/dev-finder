6174e38ce6d425335c46a5eb999d4055851befc3
giovannamoeller <giovannagmoeller@gmail.com>
2021-09-09 14:51:24 -0300

    Creating header component

---
+++ src/components/Header/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid blue;
+`;
