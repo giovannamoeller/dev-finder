35fbb62a0ddbd534e0e343e07bf19f97caa081f6
giovannamoeller <giovannagmoeller@gmail.com>
2021-09-09 19:25:59 -0300

    User not found validation

--- src/components/ProfileUser/index.js
+++ src/components/ProfileUser/index.js
@@ -4,13 +4,18 @@ import { useState, useEffect } from "react";
 
 export function ProfileUser({ username }) {
   const [userInformations, setUserInformations] = useState({});
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    console.log(username);
-
     async function fetchAPI() {
       const response = await fetch(`https://api.github.com/users/${username}`);
       const data = await response.json();
+
+      if (data.message === "Not Found") {
+        setError("User not found.");
+        return;
+      }
+      setError("");
 
       const created_at = new Date(data.created_at).toLocaleDateString("en-US", {
         year: "numeric",
@@ -35,72 +40,78 @@ export function ProfileUser({ username }) {
       });
     }
 
-    fetchAPI();
+    if (username.length > 0) fetchAPI();
   }, [username]);
 
   return (
     <ProfileUserContainer>
-      {username === "" ? (
-        <h1>Search for a GitHub User!</h1>
+      {username.length > 0 ? (
+        error.length > 0 ? (
+          <h1>{error}</h1>
+        ) : (
+          <>
+            <ImageContainer>
+              <img src={userInformations.avatar_url} alt="" />
+            </ImageContainer>
+            <InfoContainer>
+              <div className="nameInfo">
+                <div className="name">
+                  <h1>{userInformations.name}</h1>
+                  <span>{userInformations.username}</span>
+                </div>
+                <span className="date">
+                  Joined {userInformations.created_at}
+                </span>
+              </div>
+
+              <div className="profileInfo">
+                <span className="bio">{userInformations.bio}</span>
+
+                <div className="profile__infos">
+                  <div className="repos">
+                    <span>Repos</span>
+                    <strong>{userInformations.repos}</strong>
+                  </div>
+                  <div className="followers">
+                    <span>Followers</span>
+                    <strong>{userInformations.followers}</strong>
+                  </div>
+                  <div className="following">
+                    <span>Following</span>
+                    <strong>{userInformations.following}</strong>
+                  </div>
+                </div>
+
+                <div className="personal__infos">
+                  <div>
+                    <div className="location">
+                      <MapPin color="#2778FF" strokeWidth={1.5} size={24} />
+                      <span>
+                        {userInformations.location || "No location available"}
+                      </span>
+                    </div>
+                    <div className="link">
+                      <Link color="#2778FF" strokeWidth={1.5} size={24} />
+                      <span>{userInformations.blog || "No blog"}</span>
+                    </div>
+                  </div>
+                  <div>
+                    <div className="twitter">
+                      <Twitter color="#2778FF" strokeWidth={1.5} size={24} />
+                      <span>{userInformations.twitter || "No twitter"}</span>
+                    </div>
+                    <div className="company">
+                      <Briefcase color="#2778FF" strokeWidth={1.5} size={24} />
+                      <span>{userInformations.company || "No company"}</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </InfoContainer>
+          </>
+        )
       ) : (
-        <>
-          <ImageContainer>
-            <img src={userInformations.avatar_url} alt="" />
-          </ImageContainer>
-          <InfoContainer>
-            <div className="nameInfo">
-              <div className="name">
-                <h1>{userInformations.name}</h1>
-                <span>{userInformations.username}</span>
-              </div>
-              <span className="date">Joined {userInformations.created_at}</span>
-            </div>
-
-            <div className="profileInfo">
-              <span className="bio">{userInformations.bio}</span>
-
-              <div className="profile__infos">
-                <div className="repos">
-                  <span>Repos</span>
-                  <strong>{userInformations.repos}</strong>
-                </div>
-                <div className="followers">
-                  <span>Followers</span>
-                  <strong>{userInformations.followers}</strong>
-                </div>
-                <div className="following">
-                  <span>Following</span>
-                  <strong>{userInformations.following}</strong>
-                </div>
-              </div>
-
-              <div className="personal__infos">
-                <div>
-                  <div className="location">
-                    <MapPin color="#2778FF" strokeWidth={1.5} size={24} />
-                    <span>
-                      {userInformations.location || "No location available"}
-                    </span>
-                  </div>
-                  <div className="link">
-                    <Link color="#2778FF" strokeWidth={1.5} size={24} />
-                    <span>{userInformations.blog || "No blog"}</span>
-                  </div>
-                </div>
-                <div>
-                  <div className="twitter">
-                    <Twitter color="#2778FF" strokeWidth={1.5} size={24} />
-                    <span>{userInformations.twitter || "No twitter"}</span>
-                  </div>
-                  <div className="company">
-                    <Briefcase color="#2778FF" strokeWidth={1.5} size={24} />
-                    <span>{userInformations.company || "No company"}</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </InfoContainer>
-        </>
+        <h1>Search for a GitHub User!</h1>
       )}
     </ProfileUserContainer>
   );
