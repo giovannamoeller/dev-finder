a7e01ec0a6a6c6a2ae82519ba8bb3a6265aa3db9
giovannamoeller <giovannagmoeller@gmail.com>
2021-09-09 16:42:47 -0300

    Finishing profile container

--- src/components/ProfileUser/index.js
+++ src/components/ProfileUser/index.js
@@ -1,10 +1,65 @@
-import { ProfileUserContainer } from './styles';
+import { ProfileUserContainer, ImageContainer, InfoContainer } from './styles';
 import { MapPin, Link, Twitter, Briefcase  } from 'react-feather';
 
 export function ProfileUser() {
   return (
     <ProfileUserContainer>
-      
+      <ImageContainer>
+        <img src="https://avatars.githubusercontent.com/u/47362960?v=4" alt="" />
+      </ImageContainer>
+      <InfoContainer>
+        <div className="nameInfo">
+          <div className="name">
+            <h1>Giovanna Moeller</h1>
+            <span>@gimoeller</span>
+          </div>
+          <span className="date">
+            Joined 25 jan 2021.
+          </span>
+        </div>
+
+        <div className="profileInfo">
+          <span className="bio">This profile has no bio.</span>
+
+          <div className="profile__infos">
+            <div className="repos">
+              <span>Repos</span>
+              <strong>8</strong>
+            </div>
+            <div className="followers">
+              <span>Followers</span>
+              <strong>722</strong>
+            </div>
+            <div className="following">
+              <span>Following</span>
+              <strong>60</strong>
+            </div>
+          </div>
+
+          <div className="personal__infos">
+            <div>
+              <div className="location">
+                <MapPin color="#2778FF" strokeWidth={1.5} size={24}/>
+                <span>Bauru</span>
+              </div>
+              <div className="link">
+                <Link color="#2778FF" strokeWidth={1.5} size={24}/>
+                <span>https://blog.com</span>
+              </div>
+            </div>
+            <div>
+              <div className="twitter">
+                <Twitter color="#2778FF" strokeWidth={1.5} size={24}/>
+                <span>gimoeller</span>
+              </div>
+              <div className="company">
+                <Briefcase color="#2778FF" strokeWidth={1.5} size={24}/>
+                <span>Alura</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </InfoContainer>
     </ProfileUserContainer>
   )
 }
--- src/components/ProfileUser/styles.js
+++ src/components/ProfileUser/styles.js
@@ -1,12 +1,78 @@
 import styled from "styled-components";
 
 export const ProfileUserContainer = styled.section`
+  padding: 3rem;
   width: 100%;
-  border: 1px solid green;
-  height: 50vh;
+  display: flex;
+  height: 54vh;
   margin-top: 3rem;
   border-radius: 0.5rem;
   background: #1F2A48;
   box-shadow: 0px 100px 80px rgba(12, 12, 37, 0.11), 0px 64.8148px 46.8519px rgba(12, 12, 37, 0.0835185), 0px 38.5185px 25.4815px rgba(12, 12, 37, 0.0668148), 0px 20px 13px rgba(12, 12, 37, 0.055), 0px 8.14815px 6.51852px rgba(12, 12, 37, 0.0431852), 0px 1.85185px 3.14815px rgba(12, 12, 37, 0.0264815);
 `;
 
+export const ImageContainer = styled.div`
+  max-width: 20%;
+  height: 100%;
+  margin-right: 3rem;
+  img {
+    width: 100%;
+    border-radius: 50%;
+  }
+`;
+
+export const InfoContainer = styled.div`
+  height: 100%;
+  width: 80%;
+
+  & .nameInfo {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 2rem;
+    span {
+      color: #2778FF;
+      &.date {
+        color: #A4A4A4;
+      }
+    }
+  }
+
+  & .profileInfo {
+    span {
+      color: #A4A4A4;
+    }
+
+    .profile__infos {
+      margin-top: 2rem;
+      background: #151C30;
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      text-align: center;
+      padding: 1rem;
+      border-radius: 0.5rem;
+      .repos, .followers, .following {
+        display: flex;
+        flex-direction: column;
+        strong {
+          font-size: 1.5rem;
+          margin-top: 1rem;
+        }
+      }
+    }
+
+    .personal__infos {
+      margin-top: 4rem;
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+
+      div > div {
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+        margin-bottom: 2rem;
+        font-size: 0.9rem;
+      }
+    }
+  }
+`;
