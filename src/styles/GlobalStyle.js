c824415462286c1193fbe8518ce8dd3a60e93a59
giovannamoeller <giovannagmoeller@gmail.com>
2021-09-09 15:36:40 -0300

    Changing font family

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -5,7 +5,7 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'Inter', sans-serif
+    font-family: 'Space Mono', monospace;
   }
 
   body {
